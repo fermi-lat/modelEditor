7757bf5ddb2d68fa2db8f36f5e57dcf01fc1a3d6
jasercion <joseph.a.asercion@nasa.gov>
2019-09-26 11:51:44 -0400

    Update pathing

--- src/modeleditor.cxx
+++ src/modeleditor.cxx
@@ -11,6 +11,6 @@
 
 int main(int iargc, char * argv[]) {
    std::ostringstream command;
-   command << "python $INST_DIR/python/ModelEditor.py";
+   command << "python $INST_DIR/lib/python*/site-packages/fermitools/ModelEditor.py";
    std::system(command.str().c_str());
 }
